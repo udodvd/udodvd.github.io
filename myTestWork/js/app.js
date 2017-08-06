0c510a51b4dbb7b0ac12b264804c717b90f23f38
udodvd <udodvd@mail.ru>
2017-08-06 21:05:44 +0300

    sdf
    sdfsdf123

--- myTestWork/js/app.js
+++ myTestWork/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', [])
 
 
-app.controller('mainCtrl', ['$scope',
+app.controller('mainCtrl',
 		function ($scope){
 
 		$scope.editcar = {}
@@ -118,28 +118,26 @@ app.controller('mainCtrl', ['$scope',
 		$scope.sortType = 'name';
 		$scope.sortReverse = false;
 		$scope.searchFilter = '';
-}]);
+});
 
 $(document).ready(function() {
 	$('.nav li:first').tab('show') // Select first tab
 	$('#myTabs .tab-pane:first').addClass('active')
     //close Tabs animation
     $('.closeBtn').on('click',function(){
-        console.log($(this).text())
           if($(this).text() === 'Close'){
-            $(this).text('Show')
-        }else{
+              $(this).text('Show')
+            }else{
             $(this).text('Close');
-        }
+            }
         $('.slideTab').slideToggle("slow", "swing",function(){
-            console.log($('.listCars').attr('class'))
             if($('.listCars').hasClass('col-md-6')){
                 $('.listCars').addClass("col-md-12").removeClass("col-md-6");
             }else{
                 $('.listCars').addClass("col-md-6").removeClass("col-md-12");
-            }
-        
-    });
+            }   
+        });
         //END Tabs animation
-     });
+     
+    });
 });
